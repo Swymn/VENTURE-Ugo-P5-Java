45708d6afc4e1c2b9f412c7eba5139a2d41f37e9
Swymn <venture.ugo@gmail.com>
2023-12-17 22:29:27 +0100

    Update documentation pt1

--- gateway/src/main/java/fr/swynn/gateway/core/SafetyNetGateway.java
+++ gateway/src/main/java/fr/swynn/gateway/core/SafetyNetGateway.java
@@ -1,10 +1,20 @@
 package fr.swynn.gateway.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ServiceLoader;
 
 public class SafetyNetGateway implements Gateway {
 
+    private static final Logger LOGGER;
+    private static final String GATEWAY_LOADED_WITH_PROXY = "Gateway loaded with proxy: {}";
+
     private PersonServiceProxy personProxy;
+
+    static {
+        LOGGER = LoggerFactory.getLogger(SafetyNetGateway.class);
+    }
 
     public SafetyNetGateway() {
         loadPersonProxy();
@@ -13,6 +23,7 @@ public class SafetyNetGateway implements Gateway {
     private void loadPersonProxy() {
         final var proxy = ServiceLoader.load(PersonServiceProxy.class);
         personProxy = proxy.findFirst().orElseThrow();
+        LOGGER.info(GATEWAY_LOADED_WITH_PROXY, personProxy.getClass().getName());
     }
 
     @Override
